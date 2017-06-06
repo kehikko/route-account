69987bcc0c24d295ba1e361a5bca438b8ce7835b
Antti Partanen <aehparta@iki.fi>
2017-06-06 10:49:12 +0300

    first files

---
+++ controllers/AuthController.php
@@ -0,0 +1,116 @@
+<?php
+
+/******************************************************************************/
+class AuthController extends Core\Controller
+{
+	/**************************************************************************/
+	public function loginAction()
+	{
+		/* do not puth authentication entries into history */
+		$this->kernel->historyDisable();
+
+		if ($this->session->get('username'))
+		{
+			throw new RedirectException($this->route('logout'), 302);
+		}
+
+		$params                  = array();
+		$params['last_username'] = '';
+
+		if ($this->kernel->method == 'post')
+		{
+			$username     = $this->input('username');
+			$password     = $this->input('password');
+
+			$ok = $this->session->authenticate($username, $password);
+			if ($ok)
+			{
+				throw new RedirectException($this->kernel->historyPop(), 302);
+			}
+
+			$this->kernel->msg('error', 'Login failed.');
+			$params['last_username'] = $username;
+		}
+
+		return $this->render('login.html', $params);
+	}
+
+	/**************************************************************************/
+	public function logoutAction()
+	{
+		/* do not puth authentication entries into history */
+		$this->kernel->historyDisable();
+
+		if ($this->session->get('username'))
+		{
+			$this->session->destroy();
+		}
+		throw new RedirectException($this->route('frontpage:index'), 302);
+	}
+
+	/**************************************************************************/
+	public function passwordAction($username = false)
+	{
+		/* do not puth authentication entries into history */
+		$this->kernel->historyDisable();
+
+		$params = array();
+		$params['username'] = false;
+
+		/* change password if request was post */
+		if ($this->kernel->method == 'post')
+		{
+			$user          = $this->session->getUser();
+			$password_old  = $this->input('password-old');
+			$password_new1 = $this->input('password-new1');
+			$password_new2 = $this->input('password-new2');
+
+			/* if user has role:admin (or role:root for that matter)
+			 * it is possible to change passwords for other users.
+			 */
+			if ($this->session->authorize('role:admin') && $username)
+			{
+				$users = $this->session->getUsers();
+				if (!isset($users[$username])) 
+				{
+					throw new Exception('User does not exist: '.$username);
+				}
+				$user = $users[$username];
+			}
+
+			if ($password_new1 != $password_new2)
+			{
+				$this->kernel->msg('error', 'New passwords do not match.');
+			}
+			else if (!$user->checkPassword($password_old) && (!$this->session->authorize('role:admin') || $this->session->get('username') == $user->get('username')))
+			{
+				$this->kernel->msg('error', 'Invalid password.');
+			}
+			else if (strlen($password_new1) < 8)
+			{
+				$this->kernel->msg('error', 'Password must be atleast 8 characters.');
+			}
+			else
+			{
+				$user->setPassword($password_new2);
+				$this->kernel->msg('success', 'Password changed!');
+				throw new RedirectException($this->kernel->historyPop(), 302);
+			}
+		}
+
+		/* if user has role:admin (or role:root for that matter)
+		 * it is possible to change passwords for other users.
+		 */
+		if ($this->session->authorize('role:admin') && $username)
+		{
+			$params['username'] = $username;
+		}
+
+		return $this->render('password.html', $params);
+	}
+
+	public function loginFormAction()
+	{
+		return $this->render('login-form.html');
+	}
+}
---
+++ controllers/UsersController.php
@@ -0,0 +1,132 @@
+<?php
+
+class UsersController extends Core\Controller
+{
+	public function settingsAction($username = null)
+	{
+		return $this->modifyUser(false, $username);
+	}
+
+	public function createAction()
+	{
+		return $this->modifyUser(true);
+	}
+
+	private function modifyUser($create, $username = null)
+	{
+		$this->kernel->historyDisable();
+
+		/* current user as default */
+		$user = $this->session->getUser();
+
+		/* authorize as admin when username is not ours */
+		if (!$this->authorize('role:admin') && $username !== null && $username !== $user->get('username'))
+		{
+			throw new Exception403();
+		}
+
+		if ($username !== null)
+		{
+			/* modifying some other user by admin */
+			$user = $this->session->getUser($username);
+			if (!$user)
+			{
+				throw new ErrorException($this->tr('msg/error/user-nonexisting'));
+			}
+		}
+		else if ($create)
+		{
+			/* creating new user */
+			$authenticators = $this->kernel->getConfigValue('modules', 'Core\Session', 'authenticators');
+			if (count($authenticators) > 1)
+			{
+				$usertype = $authenticators[1];
+			}
+			else if (count($authenticators) > 0)
+			{
+				$usertype = $authenticators[0];
+			}
+
+			$user = new $usertype();
+			if (!$user)
+			{
+				throw new ErrorException('Invalid user type.');
+			}
+		}
+
+		$params             = $user->getUserdata();
+		$params['username'] = $user->get('username');
+
+		if ($this->kernel->method == 'post')
+		{
+			$params = $this->input('data');
+
+			/* check email */
+			if (!Validate::email($params['email']) && strlen($params['email']) > 0)
+			{
+				$this->kernel->msg('error', $this->tr('msg/error/email-invalid'));
+			}
+			else if ($create && strlen($params['username']) < 1)
+			{
+				$this->kernel->msg('error', $this->tr('msg/error/username-invalid'));
+			}
+			else
+			{
+				$ok = true;
+				if ($create)
+				{
+					$user = $user->create($params['username']);
+					if (!$user)
+					{
+						$this->kernel->msg('error', $this->tr('msg/error/user-create'));
+						$ok = false;
+					}
+				}
+
+				if ($ok)
+				{
+					/* set values */
+					foreach ($params as $key => $value)
+					{
+						$user->set($key, $value);
+					}
+
+					/* save user data */
+					$user->saveUserdata();
+
+					if ($create)
+					{
+						$this->kernel->msg('success', $this->tr('msg/success/user-create'));
+						throw new RedirectException($this->route('password_for_user', array('username' => $user->get('username'))), 302);
+					}
+					else
+					{
+						$this->kernel->msg('success', $this->tr('msg/success/user-save-settings'));
+						throw new RedirectException($this->kernel->historyPop(), 302);
+					}
+				}
+			}
+		}
+
+		$params['languages'] = $this->kernel->config['setup']['languages'];
+		$params['create']    = $create;
+
+		return $this->display('settings.html', $params);
+	}
+
+	public function fakeAction($username)
+	{
+		$this->session->fakeUser($username);
+		throw new RedirectException($this->kernel->config['urls']['base']);
+	}
+
+	public function searchFormAction($options = null)
+	{
+		$params              = array();
+		$params['formid']    = uniqid();
+		$params['users']     = $this->session->getUsers();
+		$params['searchmax'] = 5;
+		$params['options']   = $options;
+		return $this->render('users-search-form.html', $params);
+	}
+}
